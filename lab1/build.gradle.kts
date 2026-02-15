d089defd0f47dc414a5d94bb19f489620c721368
Arseniy Alhimovich <144542914+senya-2011@users.noreply.github.com>
2026-02-15 16:20:19 +0300

    test(SecCalculator): add even&periodic&match&singularity tests

--- lab1/build.gradle.kts
+++ lab1/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:6.1.3")
     testImplementation("io.kotest:kotest-assertions-core-jvm:6.1.3")
+    testImplementation("io.kotest:kotest-property:6.1.3")
 
     testImplementation("io.mockk:mockk:1.14.9")
     implementation(kotlin("stdlib-jdk8"))
